1e33eb903ae49bb951f73d12c1c9a96a8504c418
user <user@DESKTOP-6JBEF5K>
2022-04-11 12:06:56 +0800

    123

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox_ip = new System.Windows.Forms.TextBox();
+            this.textBox_port = new System.Windows.Forms.TextBox();
+            this.textBox_listenPort = new System.Windows.Forms.TextBox();
+            this.radioButton_red = new System.Windows.Forms.RadioButton();
+            this.radioButton_green = new System.Windows.Forms.RadioButton();
+            this.radioButton_blue = new System.Windows.Forms.RadioButton();
+            this.radioButton_black = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "目標IP";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(226, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "目標Port";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(419, 15);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "接聽Port";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(648, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(62, 26);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "連接";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox_ip
+            // 
+            this.textBox_ip.Location = new System.Drawing.Point(103, 12);
+            this.textBox_ip.Name = "textBox_ip";
+            this.textBox_ip.Size = new System.Drawing.Size(69, 22);
+            this.textBox_ip.TabIndex = 4;
+            // 
+            // textBox_port
+            // 
+            this.textBox_port.Location = new System.Drawing.Point(312, 12);
+            this.textBox_port.Name = "textBox_port";
+            this.textBox_port.Size = new System.Drawing.Size(69, 22);
+            this.textBox_port.TabIndex = 5;
+            // 
+            // textBox_listenPort
+            // 
+            this.textBox_listenPort.Location = new System.Drawing.Point(499, 12);
+            this.textBox_listenPort.Name = "textBox_listenPort";
+            this.textBox_listenPort.Size = new System.Drawing.Size(69, 22);
+            this.textBox_listenPort.TabIndex = 6;
+            // 
+            // radioButton_red
+            // 
+            this.radioButton_red.AutoSize = true;
+            this.radioButton_red.Location = new System.Drawing.Point(87, 55);
+            this.radioButton_red.Name = "radioButton_red";
+            this.radioButton_red.Size = new System.Drawing.Size(42, 16);
+            this.radioButton_red.TabIndex = 7;
+            this.radioButton_red.TabStop = true;
+            this.radioButton_red.Text = "Red";
+            this.radioButton_red.UseVisualStyleBackColor = true;
+            // 
+            // radioButton_green
+            // 
+            this.radioButton_green.AutoSize = true;
+            this.radioButton_green.Location = new System.Drawing.Point(213, 55);
+            this.radioButton_green.Name = "radioButton_green";
+            this.radioButton_green.Size = new System.Drawing.Size(51, 16);
+            this.radioButton_green.TabIndex = 8;
+            this.radioButton_green.TabStop = true;
+            this.radioButton_green.Text = "Green";
+            this.radioButton_green.UseVisualStyleBackColor = true;
+            // 
+            // radioButton_blue
+            // 
+            this.radioButton_blue.AutoSize = true;
+            this.radioButton_blue.Location = new System.Drawing.Point(336, 55);
+            this.radioButton_blue.Name = "radioButton_blue";
+            this.radioButton_blue.Size = new System.Drawing.Size(45, 16);
+            this.radioButton_blue.TabIndex = 9;
+            this.radioButton_blue.TabStop = true;
+            this.radioButton_blue.Text = "Blue";
+            this.radioButton_blue.UseVisualStyleBackColor = true;
+            // 
+            // radioButton_black
+            // 
+            this.radioButton_black.AutoSize = true;
+            this.radioButton_black.Location = new System.Drawing.Point(461, 55);
+            this.radioButton_black.Name = "radioButton_black";
+            this.radioButton_black.Size = new System.Drawing.Size(50, 16);
+            this.radioButton_black.TabIndex = 10;
+            this.radioButton_black.TabStop = true;
+            this.radioButton_black.Text = "Black";
+            this.radioButton_black.UseVisualStyleBackColor = true;
+            this.radioButton_black.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.radioButton_black);
+            this.Controls.Add(this.radioButton_blue);
+            this.Controls.Add(this.radioButton_green);
+            this.Controls.Add(this.radioButton_red);
+            this.Controls.Add(this.textBox_listenPort);
+            this.Controls.Add(this.textBox_port);
+            this.Controls.Add(this.textBox_ip);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox_ip;
+        private System.Windows.Forms.TextBox textBox_port;
+        private System.Windows.Forms.TextBox textBox_listenPort;
+        private System.Windows.Forms.RadioButton radioButton_red;
+        private System.Windows.Forms.RadioButton radioButton_green;
+        private System.Windows.Forms.RadioButton radioButton_blue;
+        private System.Windows.Forms.RadioButton radioButton_black;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using Microsoft.VisualBasic.PowerPacks;
+
+namespace WindowsFormsApp1
+{
+
+    public partial class Form1 : Form
+    {
+        UdpClient U;
+        Thread Th;
+        ShapeContainer C;
+        ShapeContainer D;
+        Point stP;
+        string p;
+        public Form1()
+        {
+            InitializeComponent();
+
+            
+        }
+        
+        private void Listen()
+        {
+            int Port = int.Parse(textBox_listenPort.Text);
+            U = new UdpClient(Port);
+
+            IPEndPoint EP = new IPEndPoint(IPAddress.Parse("127.0.0.1"),Port);
+            while (true)
+            {
+                byte[] B = U.Receive(ref EP);
+                string A = Encoding.Default.GetString(B);
+                string[] Z = A.Split('_');
+                string[] Q = A.Split('/');
+                Point[] R = new Point[Q.Length];
+
+                for(int i = 0; i < Q.Length; i++)
+                {
+                    string[] K = Q[i].Split(',');
+                    R[i].X = int.Parse(K[0]);
+                    R[i].Y = int.Parse(K[1]);
+
+                }
+                for (int i = 0; i < Q.Length; i++)
+                {
+                    LineShape L = new LineShape();
+                    L.StartPoint = R[i];
+                    L.EndPoint = R[i + 1];
+
+                    switch (Z[0])
+                    {
+                        case "1":
+                            L.BorderColor = Color.Red;
+                            break;
+                        case "2":
+                            L.BorderColor = Color.Lime;
+                            break;
+                        case "3":
+                            L.BorderColor = Color.Blue;
+                            break;
+                        case "4":
+                            L.BorderColor = Color.Black;
+                            break;
+                    }
+
+                    L.Parent = D;
+                }
+
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            C = new ShapeContainer(); //建立畫布
+            this.Controls.Add(C);
+            D = new ShapeContainer();
+            this.Controls.Add(D);
+        }
+        
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                Th.Abort();
+                U.Close();
+            }
+            catch
+            {
+
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Control.CheckForIllegalCrossThreadCalls = false;
+            Th = new Thread(Listen);
+            Th.Start();
+            button1.Enabled = false;
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            stP = e.Location;
+            p = stP.X.ToString() + "," + stP.Y.ToString();
+
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+            
+            LineShape L = new LineShape();
+            L.StartPoint = stP;
+            L.EndPoint = e.Location;
+
+            if (radioButton_red.Checked) { L.BorderColor = Color.Red; }
+            if (radioButton_green.Checked) { L.BorderColor = Color.Green; }
+            if (radioButton_blue.Checked) { L.BorderColor = Color.Blue; }
+            if (radioButton_black.Checked) { L.BorderColor = Color.Black; }
+
+            L.Parent = C;
+            stP = e.Location;
+            p += "/" + stP.X.ToString() + "," + stP.Y.ToString();
+            }
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            int Port = int.Parse(textBox_port.Text);
+            UdpClient S = new UdpClient(textBox_ip.Text, Port);
+
+            if (radioButton_red.Checked) { p = "1_" + p; }
+            if (radioButton_green.Checked) { p = "2_" + p; }
+            if (radioButton_blue.Checked) { p = "3_" + p; }
+            if (radioButton_black.Checked) { p = "4_" + p; }
+
+            byte[] B = Encoding.Default.GetBytes(p);
+            S.Send(B, B.Length);
+            S.Close();
+
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
